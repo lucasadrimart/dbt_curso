04e4ac7bff47500418aea1c69a5ae742f4276e82
Lucas Martins <lucasadrimart@gmail.com>
2023-11-04 02:39:15 +0000

    5

--- models/gold/dim_product.sql
+++ models/gold/dim_product.sql
@@ -1,18 +1,18 @@
 with stg_product as
     (
         select *
-        from {{ref('stg.product')}}
+        from {{ref('stg_product')}}
     ),
 stg_product_category as
     (
         select *
-        from {{ref('stg.productcategory')}}
+        from {{ref('stg_productcategory')}}
     
     ),
 stg_product_subcategory as
     (
         select *
-        from {{ref('stg.productsubcategory')}}
+        from {{ref('stg_productsubcategory')}}
     
     ),
 
@@ -21,21 +21,21 @@ transformation as
 
 
 (
-    select 
-        {{ dbt.utils.generate_surrogate_key(['stg_product_productid'])}} as product_key,
+    select
+        {{ dbt_utils.generate_surrogate_key(['stg_product.productid'])}} as product_key,
         stg_product.productid,
         stg_product.product_name,
         stg_product.productnumber,
         stg_product.color,
         stg_product.class,
-        stg_product_category.product_category_name
+        stg_product_category.product_category_name,
         stg_product_subcategory.product_subcategory_name
     from
         stg_product
         left join stg_product_subcategory PSC
-        on stg_product_productid = PSC.productsubcategoryid
+        on stg_product.productsubcategoryid = PSC.productsubcategoryid
         left join stg_product_category PC
-        on PSC.productsubcategoryid = PC.productcategoryid
+        on PSC.productcategoryid = PC.productcategoryid
 
 )
 
